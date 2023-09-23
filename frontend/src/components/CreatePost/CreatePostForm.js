4c10a60765b43fdebff5c34b1d57c376b0670822
Sean <pca.sean.jeun@gmail.com>
2023-09-23 17:45:35 -0400

    implemented character amount limitation in create post textarea

--- frontend/src/components/CreatePost/CreatePostForm.js
+++ frontend/src/components/CreatePost/CreatePostForm.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreatePostForm = () => {
+    const [inputValue, setInputValue] = useState("");
+    const maxLength = 910;
+    const handleTextareaInput = (e) => {
+        e.target.style.height = "auto";
+        e.target.style.height = e.target.scrollHeight + "px";
+    };
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        
+        if (value.length <= maxLength){
+            setInputValue(value);
+        }
+    };
 
     return (
         <form>
-            <input className="post-body-input" type="text" placeholder="What do you want to talk about?"/>
+            <textarea
+                className="post-body-input"
+                value={inputValue}
+                onChange={handleInputChange}
+                onInput={handleTextareaInput}
+                placeholder="What do you want to talk about?"
+            />
         </form>
-    )
-}
+    );
+};
 
-export default CreatePostForm
+export default CreatePostForm;
