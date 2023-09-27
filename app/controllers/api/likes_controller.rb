9b1d8ebf6cbb1f519b423a360b77614fcfa85c26
Sean <pca.sean.jeun@gmail.com>
2023-09-27 19:59:17 -0400

    like model/routes built/configured

---
+++ app/controllers/api/likes_controller.rb
@@ -0,0 +1,26 @@
+class Api::LikesController < ApplicationController
+
+    def create
+        @like = Like.new(like_params)
+        @like.liker_id = current_user.id
+        if @like.save
+            render :show
+        else
+            render json: @like.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @like = Like.find_by(id: params[:id])
+        if @like && @like.destroy
+            render :show
+        else
+            render json: @like.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def like_params
+        params.require(:like).permit(:liker_id, :post_id)
+    end
+end
